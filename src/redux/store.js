74a427fe6a29675710699192ac10bb2dee494edb
Dharmesh <dharmeshkota123@gmail.com>
2024-11-01 08:43:26 +0530

    Made changes with logger middleware for production deployment!

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,9 +4,18 @@ import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') middlewares.push(logger);
+
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }).concat(...middlewares),
 });
 
 export const persistor = persistStore(store);
